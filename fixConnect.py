f105b40b3b81d81b0739671e49729e6dc88c8531
Carl <carlylespeaks@gmail.com>
2025-03-05 18:09:29 -0500

    Update fixConnect.py

--- fixConnect.py
+++ fixConnect.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python2.7
-
 import socket
    
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-MESSAGE = "35=D,55=AAPL,38=10000,54=2,44=1,49=MYFIRM,56=FIXHUB,128=AESDESK"
+
+# Added minimum required FIX header fields and proper trailer
+MESSAGE = "8=FIX.4.2,9=103,35=D,55=AAPL,38=10000,54=2,44=1,49=MYFIRM,56=FIXHUB,128=AESDESK,10=000"
  
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
-s.close()
- 
-print "received data:", data
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))
+    s.send(MESSAGE)
+    data = s.recv(BUFFER_SIZE)
+    s.close()
+    
+    print "received data:", data
+except socket.error as e:
+    print "Socket error:", e
